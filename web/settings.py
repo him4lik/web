a83b2db6b42f137977400160f23a9d490e8011db
himalik <him4lik@gmail.com>
2024-04-16 18:27:48 +0530

    changes

--- web/settings.py
+++ web/settings.py
@@ -87,10 +87,18 @@ ASGI_APPLICATION = 'web.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DB_NAME = os.environ['DB_NAME']
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_HOST = os.environ['DB_HOST']
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': DB_NAME,
+        'USER':DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST':DB_HOST
     }
 }
 
